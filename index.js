fd052130ad1181ec6db3f43b415ca1c32cf6c2a1
anggifit <abonaldy@gmail.com>
2023-12-09 11:39:41 +0100

    show value on display

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const previousValue = document.getElementById('previousValue')
-const currentValue = document.getElementById('currentValue')
-const numberButton = document.querySelectorAll('.number')
+let previousInputDisplay = document.getElementById('previousValue')
+let currentInputDisplay = document.getElementById('currentValue')
+const numberButtons = document.querySelectorAll('.number')
 const operatorButton = document.querySelectorAll('.operator')
+const backButton = document.querySelector('.backButton')
+
+let currentValue = ''
+
+function showValue() {
+    numberButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            event.preventDefault()
+            const value = button.innerHTML
+            console.log(value) 
+            currentValue += value
+            currentInputDisplay.value = currentValue
+        })
+    }) 
+}
+showValue()
 
 const mathOperations = new MathOperations()
 
-console.log(mathOperations.div(20,2))
+
+
